a9bb28e91139e04c546c8f25ecac7364088f97e8
haruto2194 <miyasan2194@gmail.com>
2023-01-04 16:34:03 +0900

    Add 初期設定&基礎実装

--- config/app.php
+++ config/app.php
@@ -60,16 +60,16 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Application Timezone
     |--------------------------------------------------------------------------
     |
     | Here you may specify the default timezone for your application, which
     | will be used by the PHP date and date-time functions. We have gone
     | ahead and set this to a sensible default for you out of the box.
+    | Application Timezone
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Asia/Tokyo',
 
     /*
     |--------------------------------------------------------------------------
